70f77a78d6621d366e75ce4a5bb9a51b693967a7
Lars Bekhof <larsbekhof@gmail.com>
2017-11-02 12:55:29 +0100

    MOVED rendered map from server.R to render.R

--- Gemeente Amsterdam Visuals/render.R
+++ Gemeente Amsterdam Visuals/render.R
@@ -1,3 +1,4 @@
+# Render intial map
 render_map <- function() {
   return(renderLeaflet({
     leaflet(data = neightbourhood_map) %>%
@@ -24,4 +25,43 @@ render_map <- function() {
         title = statistics[1, 3]
       )
   }))
+}
+
+# Update map on a year or stat change
+update_map <- function(year, stat) {
+  query <- paste("SELECT * FROM facts WHERE year = ", year, " AND statistics_id = ", 
+                statistics[statistics$statistics_variable == stat, 1])
+  stats <- get_query(query)
+  
+  map_fact <- data.frame(locations, value = stats[match(locations$locations_id, stats$locations_id), "value"])
+  
+  map <- leafletProxy("map", data = neightbourhood_map) %>%
+    clearShapes() %>%
+    clearControls() %>%
+    addPolygons(stroke = TRUE, 
+                weight = 2, 
+                smoothFactor = 1, 
+                fillOpacity = 0.7, 
+                color = "white",
+                fillColor = ~pal(map_fact$value),
+                highlightOptions = highlightOptions(color = "red", 
+                                                    weight = 4,
+                                                    bringToFront = TRUE), 
+                label = paste(map_fact$neighbourhood_name, " - ", map_fact$value)) %>%
+    addLegend(pal = pal, values = ~map_fact$value, opacity = 0.9, title = stat)
+  
+  return(map)
+}
+
+# Display graph based on selected area on map
+show_map_graph <- function(mouse) {
+  if(is.null(mouse))
+    return()
+  text<-paste("Lattitude ", mouse$lat, "Longtitude ", mouse$lng)
+  text2<-paste("You've selected point ", mouse$id)
+  map <- leafletProxy("map") %>%
+    clearPopups() %>%
+    addPopups(mouse$lng, mouse$lat, text)
+  
+  return(map)
 }
--- Gemeente Amsterdam Visuals/server.R
+++ Gemeente Amsterdam Visuals/server.R
@@ -2,54 +2,14 @@ server <- function(input, output, session) {
   # Combine the selected variables into a new data frame
   output$map <- render_map()
   
-  observe({
-    temp <- paste("SELECT * FROM facts WHERE year = ", input$year, " AND statistics_id = ", 
-                  statistics[statistics$statistics_variable == input$stat, 1])
-    con <- dbConnect(RMySQL::MySQL(), 
-                     dbname = "zbekhofl001",
-                     host = "oege.ie.hva.nl", 
-                     user = "bekhofl001", 
-                     password = "ovv2atL4IgRBjd", 
-                     client.flag = CLIENT_MULTI_STATEMENTS)
-    dbListTables(con)
-    
-    stats <- dbGetQuery(con, temp)
-    
-    dbDisconnect(con)
-    
-    map_fact <- data.frame(locations, value = stats[match(locations$locations_id, stats$locations_id), "value"])
-    
-    leafletProxy("map", data = neightbourhood_map) %>%
-      clearShapes() %>%
-      clearControls() %>%
-      addPolygons(stroke = TRUE, 
-                  weight = 2, 
-                  smoothFactor = 1, 
-                  fillOpacity = 0.7, 
-                  color = "white",
-                  fillColor = ~pal(map_fact$value),
-                  highlightOptions = highlightOptions(color = "red", 
-                                                      weight = 4,
-                                                      bringToFront = TRUE), 
-                  label = paste(map_fact$neighbourhood_name, " - ", map_fact$value)) %>%
-      addLegend(pal = pal, values = ~map_fact$value, opacity = 0.9, title = input$stat)
-  })
+  observeEvent({
+    input$stat
+    input$year
+    }, update_map(input$year, input$stat))
   
   output$dynamicsidebar <- renderMenu({
-    sidebarMenu(
-      
-    )
+    sidebarMenu()
   })
   
-  observeEvent(input$map_shape_click, { # update the location selectInput on map clicks
-    p <- input$map_shape_click
-    if(is.null(p))
-      return()
-    text<-paste("Lattitude ", p$lat, "Longtitude ", p$lng)
-    text2<-paste("You've selected point ", p$id)
-    leafletProxy("map") %>%
-      clearPopups() %>%
-      addPopups( p$lat, p$lng, text)
-  })
-  
+  observeEvent(input$map_shape_click, show_map_graph(input$map_shape_click))
 }
