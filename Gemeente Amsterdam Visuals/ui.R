be0bb30ef85a10474b9a768d225448203257f5ae
Lars Bekhof <larsbekhof@gmail.com>
2017-11-23 11:39:00 +0100

    ADDED theme css file, slider filters in sidebar

--- Gemeente Amsterdam Visuals/ui.R
+++ Gemeente Amsterdam Visuals/ui.R
@@ -3,49 +3,76 @@ source("mysql.R")
 source("init.R")
 source("render.R")
 
-ui <- dashboardPage( skin = "red",
+ui <- dashboardPage(
+  skin = "red",
   dashboardHeader(title = "Amsterdam Statistieken", titleWidth = 300),
-  dashboardSidebar( width = 300,
-    sidebarMenu( id = "sidebar",
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Interactieve map", tabName = "map", icon = icon("map")),
-      menuItem("Charts", tabName = "chart", icon = icon("bar-chart-o"),
-               menuSubItem("Chart 1", tabName = "subchart1"),
-               menuSubItem("Chart 2", tabName = "subchart2")
-               ),
+  dashboardSidebar(
+    width = 300,
+    sidebarMenu(
+      id = "sidebar",
+      menuItem(
+        "Dashboard",
+        tabName = "dashboard",
+        icon = icon("dashboard")
+      ),
+      menuItem(
+        "Interactieve map",
+        tabName = "map",
+        icon = icon("map")
+      ),
+      conditionalPanel(
+        condition = "input.sidebar == 'map'",
+        class = "filter-panel",
+        selectInput(
+          "stat",
+          "Select statistic",
+          split(statistics$statistics_variable, statistics$statistics_name)
+        ),
+        sliderInput("year", "Select year", 2005, 2017, 2017, step = 1)
+      ),
+      menuItem(
+        "Charts",
+        tabName = "chart",
+        icon = icon("bar-chart-o"),
+        menuSubItem("Chart 1", tabName = "subchart1"),
+        menuSubItem("Chart 2", tabName = "subchart2")
+      ),
       menuItem("Data explorer", tabName = "datatable", icon = icon("table")),
-      sidebarMenuOutput("dynamicsidebar")
+      conditionalPanel(condition = "input.sidebar == 'chart'", h2("Test")),
+      conditionalPanel(condition = "input.sidebar == 'datatable'", h3("Test"))
     )
   ),
   dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "theme.css")
+    ),
     tabItems(
-      tabItem( tabName = "dashboard",
-               h2("Dashboard")
-      ),
-      tabItem( tabName = "map",
-               
+      tabItem(tabName = "dashboard",
+              h2("Dashboard")),
+      tabItem(
+        tabName = "map",
+        
         fluidRow(
-            tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
-            leafletOutput("map", width = "100%"),
-
-            absolutePanel(id = "map_graph_panel", bottom = "20", left = "320", width = 500, height = 400,
-                          plotOutput("map_graph")),
-            absolutePanel(id = "control", top = 700, right = 50,
-                          selectInput("stat", "Select statistic", split(statistics$statistics_variable, statistics$statistics_name)),
-                          sliderInput("year", "Select year", 2005, 2017, 2017, step = 1)
-            ),
-          tags$style(type='text/css', ".selectize-dropdown-content {max-height: 150px; }")
-         )
+          tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+          leafletOutput("map", width = "100%"),
+          
+          absolutePanel(
+            id = "map_graph_panel",
+            bottom = "20",
+            left = "320",
+            width = 500,
+            height = 400,
+            plotOutput("map_graph")
+          ),
+          tags$style(type = 'text/css', ".selectize-dropdown-content {max-height: 150px; }")
+        )
       ),
-      tabItem( tabName = "subchart1",
-               h2("Chart 1")
-      ),
-      tabItem( tabName = "subchart2",
-               h2("Chart 2")
-      ),
-      tabItem( tabName = "datatable",
-               h2("Data explorer")
-      )
+      tabItem(tabName = "subchart1",
+              h2("Chart 1")),
+      tabItem(tabName = "subchart2",
+              h2("Chart 2")),
+      tabItem(tabName = "datatable",
+              h2("Data explorer"))
     )
     
   )
