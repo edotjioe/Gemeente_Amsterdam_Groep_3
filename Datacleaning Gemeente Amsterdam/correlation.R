0d92f408995efda427872d0473d57e70bce2dadd
Lars Bekhof <larsbekhof@gmail.com>
2018-01-11 14:57:05 +0100

    More correlation code - STILL BROKEN

--- Datacleaning Gemeente Amsterdam/correlation.R
+++ Datacleaning Gemeente Amsterdam/correlation.R
@@ -1,8 +1,8 @@
 #install.packages("readxl")
-install.packages("dplyr")
+#install.packages("dplyr")
 #install.packages("stringr")
-install.packages("DBI")
-install.packages("RMySQL")
+#install.packages("DBI")
+#install.packages("RMySQL")
 # library(readxl)
 library(dplyr)
 # library(stringr)
@@ -18,8 +18,84 @@ con <- dbConnect(RMySQL::MySQL(),
 
 statistics <- dbGetQuery(con, "SELECT * FROM statistics")
 facts <- dbGetQuery(con, "SELECT * FROM facts")
-#rm(statistics)
+locations <- dbGetQuery(con, "SELECT * from locations")
 
+dbDisconnect(con)
+
+# --- Create a data frame with all ---
+correlations <- data.frame(
+  correlations_id = numeric(0),
+  statistics_1_id = numeric(0),
+  statistics_2_id = numeric(0),
+  district_code = character(0),
+  value = numeric(0))
+
+stat_ids <- statistics$statistics_id
+for(i in 1:length(stat_ids)) {
+  statistics_1_id <- rep(stat_ids[i], length(stat_ids) - 1)
+  statistics_2_id <- setdiff(stat_ids, stat_ids[i])
+  
+  df <- data.frame(
+    correlations_id = 0,
+    statistics_1_id = statistics_1_id,
+    statistics_2_id = statistics_2_id,
+    district_code = character(length(statistics_1_id)),
+    value = numeric(length(statistics_1_id)))
+  correlations <- rbind(correlations, df)
+}
+# --- ---
+
+# --- Duplicate the earlier created data frame for every district ---
+location_ids <- unique(locations$district_code)
+copy <- correlations
+correlations <- correlations[!1:nrow(correlations),]
+for(i in 1:length(location_ids)) {
+  copy$district_code <- as.character(location_ids[i])
+  correlations <- rbind(correlations, copy)
+}
+# --- ---
+
+# Assign an id to each correlation
+correlations$correlations_id <- 1:nrow(correlations)
+
+for(i in 1:1000) { #nrow(correlations)
+  i <- 39
+  locations_id <- locations[locations$district_code == correlations[i,]$district_code,]$locations_id
+
+  if(!(correlations[i,]$statistics_1_id %in% facts$statistics_id) | !(correlations[i,]$statistics_1_id %in% facts$statistics_id)) {next}
+  
+  cor1 <- facts[facts$statistics_id == correlations[i,]$statistics_1_id,]
+  cor2 <- facts[facts$statistics_id == correlations[i,]$statistics_2_id,]
+  
+  cor1 <- cor1[locations_id %in% cor1$locations_id,]
+  cor2 <- cor2[locations_id %in% cor2$locations_id,]
+
+  if(nrow(cor1) > nrow(cor2)) {
+    cor1 <- cor1[cor2$locations_id %in% cor1$locations_id,]
+  } else if(nrow(cor1) < nrow(cor2)) {
+    cor2 <- cor2[cor1$locations_id %in% cor2$locations_id,]
+  }
+  
+  if(nrow(cor1) == 0 | nrow(cor2) == 0) {next}
+  
+  cor1 %>%
+    arrange(desc(year))
+  
+  cor2 %>%
+    arrange(desc(year))
+
+  if(nrow(cor1) > nrow(cor2)) {
+    cor1 <- cor1[cor2$year %in% cor1$year,]
+  } else if(nrow(cor1) < nrow(cor2)) {
+    cor2 <- cor2[cor1$year %in% cor2$year,]
+  }
+  
+  print(cor(x = cor1$value, y = cor2$value))
+  correlations[i, "value"] <- cor(x = cor1$value, y = cor2$value)
+}
+
+
+# Old code
 corr_ma <- matrix(ncol = length(statistics$statistics_id), nrow = length(statistics$statistics_id))
 
 f1 <- facts %>% 
@@ -35,8 +111,9 @@ for (i in 1:nrow(statistics)) {
     }
   }
 }
-cor(c(1,2,3,4), c(1,2,3,4))
-f1[f1$statistics_id == 104, "value"]
-df1 <- statistics[match(statistics[1,"statistics_id"], f1$statistics_id, nomatch = 0), "value"]
-df2 <- statistics[match(statistics[4,"statistics_id"], f1$statistics_id, nomatch = 0), "value"]
-cor(x = df1$value, y = df2$value)
+
+# cor(c(1,2,3,4), c(1,2,3,4))
+# f1[f1$statistics_id == 104, "value"]
+# df1 <- statistics[match(statistics[1,"statistics_id"], f1$statistics_id, nomatch = 0), "value"]
+# df2 <- statistics[match(statistics[4,"statistics_id"], f1$statistics_id, nomatch = 0), "value"]
+# cor(x = df1$value, y = df2$value)
